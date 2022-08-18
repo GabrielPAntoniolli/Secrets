809854f9869b2e9a02daaa618be0be4963e24f30
Gabriel <gabriel.pa16@gmail.com>
2022-08-18 08:11:42 +0100

    Authentication with hash and salting using bcrypt

--- app.js
+++ app.js
@@ -4,28 +4,28 @@ const express = require( "express" );
 const bodyParser = require( "body-parser" );
 const ejs = require( "ejs" );
 const mongoose = require( "mongoose" );
-const encrypt = require( "mongoose-encryption" );
+const bcrypt = require( "bcrypt" );
+const saltRounds = 10;
 
 const app = express();
 
+//DB Connection, schema creation and model( collection )
 mongoose.connect( "mongodb://localhost:27017/userDb" );
 
 const userSchema = new mongoose.Schema({
 
     email: String,
     password: String
-})
-
-
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"] });
-
+});
 const User = new mongoose.model( "User", userSchema );
 
+//express, ejs and body parser settings
 app.use( express.static( "public" ));
 app.set( "view engine", "ejs" );
 app.use( bodyParser.urlencoded( 
     {extended: true }
 ));
+
 
 app.get( "/", function( req, res ){
 
@@ -51,9 +51,14 @@ app.post( "/login", function( req, res ){
             } else {
 
                 if( foundUser ){
-                    if( foundUser.password === password ){
-                        res.render( "secrets")
-                    }
+                    bcrypt.compare(password, foundUser.password, function( err, result ) {
+
+                        if(result === true){
+                            res.render( "secrets");
+                        }
+                    })
+                        
+                    
                 }
             }
         })
@@ -71,17 +76,23 @@ app.listen( 3000, function(){
 
 app.post( "/register", function( req, res ){
 
-    const newUser = new User({
-        email: req.body.username,
-        password: req.body.password
-    });
+    bcrypt.hash(req.body.password, saltRounds, function( err, hash ){
+        
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        });
+    
+        newUser.save( function( err ){
+    
+            if( err ){
+                console.log( err );
+            } else {
+                res.render( "secrets" )
+            }
+        });
 
-    newUser.save( function( err ){
+    })
 
-        if( err ){
-            console.log( err );
-        } else {
-            res.render( "secrets" )
-        }
-    });
+    
 })
