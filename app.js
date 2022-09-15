bf21e4de027cbdb419eb15350bda910dda87cd65
Gabriel <gabriel.pa16@gmail.com>
2022-09-15 12:30:17 +0100

    inserting google login

--- app.js
+++ app.js
@@ -50,6 +50,7 @@ passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
   callbackURL: "http://www.example.com/auth/google/secrets"
+  //userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
 },
 function(accessToken, refreshToken, profile, cb) {
   User.findOrCreate({ googleId: profile.id }, function (err, user) {
